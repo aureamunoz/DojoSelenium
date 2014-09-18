aa9fcc25aa3e20c686dca74accf9f9fb5865477a
aureamunoz <auritamh80@gmail.com>
2014-09-18 14:01:26 +0200

    Add some comments

--- src/test/java/org/duchess/selenium/part1/Exercice0.java
+++ src/test/java/org/duchess/selenium/part1/Exercice0.java
@@ -2,10 +2,6 @@ package org.duchess.selenium.part1;
 
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.IOException;
 
@@ -15,33 +11,22 @@ import java.io.IOException;
 public class Exercice0
 
 {
+    /**
+     * Ouvrir le navigateur
+     * Aller sur une page
+     * Regarder le titre
+     */
+
+
     @Test
-    public void when_go_to_home_then_home_page_is_on_screen() throws IOException {
+    public void go_to_google_page() throws IOException {
 
 
 
    }
 
 
-    /**
-     * Ouvrir le navigateur
-     Aller sur google.com
-     Mettre “Cheese” dans la recherche
-     Valider
-     Regarder le titre
-     */
 
 
-    @Test
-    public void searchOnGoogleTest(){
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://www.google.com");
-        WebElement search = driver.findElement(By.name("q"));
-        search.sendKeys("cheese");
-        search.submit();
-
-        System.out.println("Page title: "+driver.getTitle());
-
-    }
 
 }
--- src/test/java/org/duchess/selenium/part3/ex2/PortofolioPage.java
+++ src/test/java/org/duchess/selenium/part3/ex2/PortofolioPage.java
@@ -5,4 +5,5 @@ package org.duchess.selenium.part3.ex2;
  * Utiliser des WebElement
  */
 public class PortofolioPage {
+    //private final WebDriver driver;
 }
